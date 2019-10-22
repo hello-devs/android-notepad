9649d3d78c541b7f449bd83f63c48bf638cb51b4
hello-devs <cirer972@gmail.com>
2019-10-22 10:13:41 +0200

    Rename MainActivity to NoteListActivity

--- app/src/main/java/com/hellodevs/training/notepad/NoteListActivity.kt
+++ app/src/main/java/com/hellodevs/training/notepad/NoteListActivity.kt
@@ -3,10 +3,12 @@ package com.hellodevs.training.notepad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note_list)
+
+
     }
 }
