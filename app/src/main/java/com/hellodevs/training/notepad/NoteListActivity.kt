f777afd823a1322352249858778e842758a0766d
hello-devs <cirer972@gmail.com>
2019-10-22 15:25:10 +0200

    Implementation recyclerview et listener dans NoteListActivity

--- app/src/main/java/com/hellodevs/training/notepad/NoteListActivity.kt
+++ app/src/main/java/com/hellodevs/training/notepad/NoteListActivity.kt
@@ -2,13 +2,38 @@ package com.hellodevs.training.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_note_list.*
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
+        notes = mutableListOf<Note>()
 
+        notes.add(Note("note 1", "ceci est la note numéro 1"))
+        notes.add(Note("note 2", "ceci est la note numéro 2"))
+        notes.add(Note("note 3", "ceci est la note numéro 3"))
+        notes.add(Note("note 4", "ceci est la note numéro 4"))
+
+        adapter = NoteAdapter(notes, this)
+
+        notes_recycler_view.layoutManager = LinearLayoutManager(this)
+        notes_recycler_view.adapter = adapter
     }
+
+    override fun onClick(view: View) {
+        if(view.tag != null){
+            Log.i("NoteListActivity","click sur une ${view.tag} note de la liste")
+        }
+    }
+
+
 }
