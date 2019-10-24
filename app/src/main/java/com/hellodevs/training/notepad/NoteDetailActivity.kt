74334e93fb23acc185d7b321c236233ccea538cf
hello-devs <cirer972@gmail.com>
2019-10-24 11:36:29 +0200

    Creation menu et option de sauvegarde
    creation menu dans res/menu
    Ajout item de sauvegarde dans le menu avec icone de material.io
    
    Integration du menu dans NoteDetailActivity en surchargeant
    onCreateOptionMenu
    Detection de la selection d'item en surchargeant
    onOptionsItemSelected

--- app/src/main/java/com/hellodevs/training/notepad/NoteDetailActivity.kt
+++ app/src/main/java/com/hellodevs/training/notepad/NoteDetailActivity.kt
@@ -2,6 +2,8 @@ package com.hellodevs.training.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_note_detail.*
 
@@ -34,5 +36,20 @@ class NoteDetailActivity : AppCompatActivity() {
 
         titleView.text = note.title
         textView.text = note.text
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_note_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_save -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
     }
 }
